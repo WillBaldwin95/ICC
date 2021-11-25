d20ebb0fa10df885254841ae6577e62aa788f84a
Will Baldwin <will2.baldwin@live.uwe.ac.uk>
2021-11-25 12:03:22 +0000

    Update sketch.js

--- Week 6/p5/Task 2/sketch.js
+++ Week 6/p5/Task 2/sketch.js
@@ -2,16 +2,29 @@ let myArr = [];
 let mySentence = "I like/love creative coding, so far";
 let chopper;
 let likelove;
+let xPos = 0;
 
 function setup(){
   createCanvas(800,200);
   frameRate(2);
   background(255);
   chopper = mySentence.split(" ");
+  likeLove = chopper[1].split("/");
+  chopper[1] = likeLove[1];
   //console.log(chopper);
-  chopper = chopper.reverse();
-  chopper = chopper.join(" ");
+  //chopper = chopper.reverse();
+  //chopper = chopper.join(" ");
+  //chopper = shuffle(chopper);
 }
 function draw(){
-  text(mySentence, 50, 100)
+  background(255);
+  for (let i=0; i<chopper.length; i++) {
+
+    text(chopper[i], xPos, random(10,height));
+    xPos = xPos+(width/1000);
+    if(xPos > width){
+      xPos = 0;
+    }
+  }
+  
 }

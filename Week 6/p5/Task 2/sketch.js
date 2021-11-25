829502bd0bf4d4f1897c1b69acc3031290da65f5
Will Baldwin <will2.baldwin@live.uwe.ac.uk>
2021-11-25 11:35:25 +0000

    Update sketch.js

--- Week 6/p5/Task 2/sketch.js
+++ Week 6/p5/Task 2/sketch.js
@@ -6,7 +6,11 @@ let likelove;
 function setup(){
   createCanvas(800,200);
   frameRate(2);
-  background(255)
+  background(255);
+  chopper = mySentence.split(" ");
+  //console.log(chopper);
+  chopper = chopper.reverse();
+  chopper = chopper.join(" ");
 }
 function draw(){
   text(mySentence, 50, 100)
